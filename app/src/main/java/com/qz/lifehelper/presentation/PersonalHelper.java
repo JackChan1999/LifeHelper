95892e2fc17642dc60127abbfff1559b8cc9ec51
kohoh <qxw2012@hotmail.com>
2015-03-28 14:39:20 +0800

    重构Personal

---
+++ app/src/main/java/com/qz/lifehelper/presentation/PersonalHelper.java
@@ -0,0 +1,40 @@
+package com.qz.lifehelper.presentation;
+
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.RootContext;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by kohoh on 15/3/23.
+ */
+
+@EBean
+public class PersonalHelper {
+
+	@RootContext
+	Context context;
+
+	/**
+	 * 前往我发布到商品页面
+	 */
+	public void toMySale() {
+		Toast.makeText(context, "前往我发布的商品页面", Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * 前往我发布的信息页面
+	 */
+	public void toMyPublish() {
+		Toast.makeText(context, "前往我发布的信息页面", Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * 前往公共信息页面
+	 */
+	public void toPublicInf() {
+		Toast.makeText(context, "前往公共信息页面", Toast.LENGTH_SHORT).show();
+	}
+
+}
--- app/src/main/java/com/qz/lifehelper/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/qz/lifehelper/ui/fragment/PersonalFragment.java
@@ -1,5 +1,6 @@
 package com.qz.lifehelper.ui.fragment;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
@@ -12,91 +13,141 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.qz.lifehelper.R;
-import com.qz.lifehelper.presentation.IPersonalView;
-import com.qz.lifehelper.presentation.PersonalFragmentPresentation;
+import com.qz.lifehelper.business.AppBusiness;
+import com.qz.lifehelper.business.AuthenticationBusiness;
+import com.qz.lifehelper.event.GetAuthEvent;
+import com.qz.lifehelper.presentation.PersonalHelper;
 
 /**
  * Created by kohoh on 15/3/23.
  */
 
 @EFragment(R.layout.fragment_personal)
-public class PersonalFragment extends Fragment implements IPersonalView{
+public class PersonalFragment extends Fragment {
 
 	@Bean
-	PersonalFragmentPresentation presentation;
+	PersonalHelper personalHelper;
+
+	@Bean
+	AuthenticationBusiness authenticationBusiness;
+
+	@Bean
+	AppBusiness appBusiness;
 
 	@Click(R.id.login_bn)
-	public void onLoginBnClick() {
-		presentation.toLogin();
+	void onLoginBnClick() {
+		// 前往登录注册页面
+		authenticationBusiness.login();
 	}
 
 	@Click(R.id.my_sales)
-	public void onMySalesClick() {
-		presentation.toMySale();
+	void onMySalesClick() {
+		personalHelper.toMySale();
 	}
 
 	@Click(R.id.my_publish)
-	public void onMyPublishClick() {
-		presentation.toMyPublish();
+	void onMyPublishClick() {
+		personalHelper.toMyPublish();
 	}
 
 	@Click(R.id.public_info)
-	public void onPublicInfoClick() {
-		presentation.toPublicInf();
+	void onPublicInfoClick() {
+		personalHelper.toPublicInf();
 	}
 
 	@Click(R.id.logout_bn)
-	public void onLogoutBnClick() {
-		presentation.logout();
+	void onLogoutBnClick() {
+		authenticationBusiness.logout();
 	}
 
-    @Override
-    public void onStart() {
-        presentation.bind(this);
-        super.onStart();
-    }
+	/**
+	 * 设置当前的用户状态
+	 */
+	@AfterViews
+	void setAuthState() {
+		if (authenticationBusiness.isLogin()) {
+			// TODO 设置状态为登录
+		} else {
+			// TODO 设置状态为未登录
+		}
+	}
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        presentation.unbind();
-    }
+	@Override
+	public void onStart() {
+		super.onStart();
+		registerEventBus();
+	}
 
-    @ViewById(R.id.login_bn)
-    View loginBn;
+	@Override
+	public void onStop() {
+		super.onStop();
+		unregisterEventBus();
+	}
 
-    @ViewById(R.id.user_icon_iv)
-    ImageView userIconIv;
+	private void registerEventBus() {
+		authenticationBusiness.getEventBus().register(this);
+	}
 
-    @ViewById(R.id.user_icon_background_iv)
-    ImageView userIconBagIv;
+	private void unregisterEventBus() {
+		authenticationBusiness.getEventBus().unregister(this);
+	}
 
-    @ViewById(R.id.user_name)
-    TextView userNameTv;
+	/**
+	 * 收到登录或者登出消息后，刷新页面的用户状态
+	 */
+	public void onEventMainThread(GetAuthEvent event) {
+		switch (event.authState) {
+		case LOGIN:
+			// TODO 设置状态为登录
+			break;
+		case LOGOUT:
+			// TODO 设置状态为未登录
+			break;
+		}
+	}
 
-    @Override
-    public void login(String userName,Bitmap userIcon, Bitmap userIconBag) {
-        userNameTv.setVisibility(View.VISIBLE);
-        loginBn.setVisibility(View.GONE);
-        userIconBagIv.setImageBitmap(userIconBag);
-        userIconIv.setImageBitmap(userIcon);
-        userNameTv.setText(userName);
-    }
+	@ViewById(R.id.login_bn)
+	View loginBn;
 
-    @Override
-    public void logout(Bitmap userIcon,Bitmap userIconBag) {
-        loginBn.setVisibility(View.VISIBLE);
-        userNameTv.setVisibility(View.GONE);
-        userIconBagIv.setImageBitmap(userIconBag);
-        userIconIv.setImageBitmap(userIcon);
-        userNameTv.setText(null);
-    }
+	@ViewById(R.id.user_icon_iv)
+	ImageView userIconIv;
 
-    @ViewById(R.id.app_version_tv)
-    TextView appVersionTv;
+	@ViewById(R.id.user_icon_background_iv)
+	ImageView userIconBagIv;
 
-    @Override
-    public void setAppVersion(String version) {
-        appVersionTv.setText(version);
-    }
+	@ViewById(R.id.user_name)
+	TextView userNameTv;
+
+	/**
+	 * 修改为登录状态
+	 */
+	void login(String userName, Bitmap userIcon, Bitmap userIconBag) {
+		userNameTv.setVisibility(View.VISIBLE);
+		loginBn.setVisibility(View.GONE);
+		userIconBagIv.setImageBitmap(userIconBag);
+		userIconIv.setImageBitmap(userIcon);
+		userNameTv.setText(userName);
+	}
+
+	/**
+	 * 修改为登出状态
+	 */
+	void logout(Bitmap userIcon, Bitmap userIconBag) {
+		loginBn.setVisibility(View.VISIBLE);
+		userNameTv.setVisibility(View.GONE);
+		userIconBagIv.setImageBitmap(userIconBag);
+		userIconIv.setImageBitmap(userIcon);
+		userNameTv.setText(null);
+	}
+
+	@ViewById(R.id.app_version_tv)
+	TextView appVersionTv;
+
+	/**
+	 * 设置当前版本号
+	 */
+	@AfterViews
+	void setAppVersion() {
+		appVersionTv.setText(appBusiness.getVersionNumber());
+	}
 }
