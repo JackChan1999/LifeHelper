49a3a38d55a56d7aecea90d5f134ca444aa93f60
kohoh <qxw2012@hotmail.com>
2015-06-05 12:49:53 +0800

    修改聚合数据appkey

--- app/src/main/java/com/qz/lifehelper/service/JuheConstant.java
+++ app/src/main/java/com/qz/lifehelper/service/JuheConstant.java
@@ -12,17 +12,17 @@ public class JuheConstant {
     public static final String QUERY_DATE_FORMAT_PATTERN = "yyyy'-'MM'-'dd";
     public static final String RESPONSE_DATE_FORMAT_PATTERN = "yyyy'-'MM'-'dd hh:mm";
 
-    static private final String PLANE_key = "06753a2070829c649753320a490c5b19";
+    static private final String PLANE_key = "16ea998d117e8396db8ec655bd75d38f";
     static private final String PLANE = "/plan";
     static public final String AIPORT = PLANE + "/city?key=" + PLANE_key + DTYPE;
     public static final String PLANE_INFO = PLANE + "/bc?key=" + PLANE_key + DTYPE;
 
-    private static final String TRAIN_KEY = "781db56ed179991a4e62449a87dba8cd";
+    private static final String TRAIN_KEY = "48477366563bf2e6539ba7d3078914c7";
     private static final String TRAIN = "/train";
     public static final String TRAIN_STATION = TRAIN + "/station.list.php?key=" + TRAIN_KEY + DTYPE;
     public static final String TRAIN_INFO = TRAIN + "/yp?key=" + TRAIN_KEY + DTYPE;
 
-    private static final String BUS_KEY = "8c12674c63e8804a8b71b748c9392687";
+    private static final String BUS_KEY = "2f9d0d99e9a2624ec351118b237ac023";
     private static final String BUS = "/bus";
     public static final String BUS_INFO = BUS + "/query_ab?key=" + BUS_KEY + DTYPE;
 }
